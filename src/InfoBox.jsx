1d90c41b5ae01ddc296b18f34992c56b6e845845
prabhat-kushwah2003 <prabhatkushwah2003@email.com>
2025-06-01 09:59:29 +0530

    update

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -19,7 +19,7 @@ export default function InfoBox({ info }) {
     "https://images.unsplash.com/photo-1540458398062-9bbc39e99010?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFpbmV5JTIwd2VhdGhlciUyMGNpdHl8ZW58MHx8MHx8fDA%3D";
 
   return (
-    <div className="infoBox">
+    <div className="InfoBox">
       {/* <h1>WeatherInfo - {info.weather}</h1> */}
       <div className="cardContainer">
         <Card sx={{ maxWidth: 345 }}>
